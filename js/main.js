302f53377543dbb0803eadf131290b04443a49ae
Alexa-2k <109705749+Alexa-2k@users.noreply.github.com>
2025-07-06 01:59:19 -0300

    trabajando sobre menú hamburguesa

--- js/main.js
+++ js/main.js
@@ -17,3 +17,29 @@ document.addEventListener("DOMContentLoaded", () => {
   incluirHTML("footer", "components/footer.html");
  
 });
+
+
+document.addEventListener("scroll", function () {
+	const nav = document.querySelector(".main-nav");
+	if (window.scrollY > 50) {
+		nav.classList.add("scrolled");
+	} else {
+		nav.classList.remove("scrolled");
+	}
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+	const toggle = document.querySelector(".menu-toggle");
+	const menu = document.querySelector(".nav-menu");
+
+	toggle.addEventListener("click", () => {
+		menu.classList.toggle("show");
+	});
+});
+
+const toggleButton = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+toggleButton.addEventListener("click", () => {
+	navMenu.classList.toggle("show");
+});
